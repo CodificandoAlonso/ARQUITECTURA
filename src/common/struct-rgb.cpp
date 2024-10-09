26668fcaf34b88f98ebc2e6003906fc964b644d3
Hector <100495794@alumnos.uc3m.es>
2024-10-09 19:21:30 +0200

    Modificacion de cosas que dijo el profe(no he borrado las cosas, solo movido a txt). Creacion de estructuras rgb y de arrays(incompleto). Modificacion de cmake de common(miralo beto que ns si lo he hecho bien)

---
+++ src/common/struct-rgb.cpp
@@ -0,0 +1,28 @@
+//
+// Created by hector on 9/10/24.
+//
+
+
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+struct rgb_min {
+    unsigned char r;
+    unsigned char g;
+    unsigned char b;
+};
+
+struct rgb_max {
+    unsigned short r;
+    unsigned short g;
+    unsigned short b;
+
+};
+
+struct soa_rgb(maxlevel) {
+    ...
+
+};
---
+++ src/common/struct-rgb.hpp
@@ -0,0 +1,20 @@
+//
+// Created by hector on 9/10/24.
+//
+
+#ifndef STRUCT_RGB_HPP
+#define STRUCT_RGB_HPP
+
+struct rgb_min {
+    unsigned char r;
+    unsigned char g;
+    unsigned char b;
+};
+
+struct rgb_max {
+    unsigned short r;
+    unsigned short g;
+    unsigned short b;
+};
+
+#endif //STRUCT_RGB_HPP
